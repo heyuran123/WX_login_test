bbd6dff5a90ace503752fe4e2a0554571342a089
heyuran123 <269070961@qq.com>
2017-11-24 11:06:27 +0800

    fix

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
  * @Author: yuran
  * @Date:   2017-11-23 17:13:16
  * @Last Modified by:   yuran
- * @Last Modified time: 2017-11-24 10:23:18
+ * @Last Modified time: 2017-11-24 11:05:59
  */
-$appid = 'wx534200b90641e999';
+$appid = 'wx8a584b7982c81418';
 
 $url = "https://open.weixin.qq.com/connect/qrconnect?appid=$appid&redirect_uri=http://www.wxlogin.com/wxlogin.php&response_type=code&scope=snsapi_login&state=1&connect_redirect=1#wechat_redirect";
 
