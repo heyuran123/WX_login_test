0725d12c99a2ea0036cfaa161f157701a55fcdbb
heyuran123 <269070961@qq.com>
2018-06-25 09:07:19 +0800

    Revert "95lian"
    This reverts commit d5363195cad8f48c9e6df2b66191806637ce4d90.

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
  * @Author: yuran
  * @Date:   2017-11-23 17:13:16
  * @Last Modified by:   yuran
- * @Last Modified time: 2017-11-28 09:38:55
+ * @Last Modified time: 2017-11-28 09:37:00
  */
 $appid = 'wx92b12a4a0cfd1933';
 $redirect_uri = 'http://www.wxlogin.com/wxlogin.php';
 
 //$url = "https://open.weixin.qq.com/connect/qrconnect?appid=$appid&redirect_uri=http://www.wxlogin.com/wxlogin.php&response_type=code&scope=snsapi_login&state=1&connect_redirect=1#wechat_redirect";
-$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect"
 
 header('location:'.$url);
